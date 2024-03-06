a07308cfb9b98dca1ba3815b80b935300e5c176e
Jan Sváček <jan@svacek.eu>
2024-03-06 22:28:13 +0100

    design changes

--- resources/js/Components/CryptoCurrency.vue
+++ resources/js/Components/CryptoCurrency.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="p-6 flex flex-col md:flex-row items-center md:justify-between relative">
-    <!-- Left Section: Crypto Information -->
     <div class="mb-4 md:mb-0 md:mr-4 text-white md:w-1/2 lg:w-2/3 xl:w-3/4">
       <div class="flex items-center justify-between">
         <div class="flex items-center">
@@ -12,26 +11,23 @@
         </div>
       </div>
       <div class="text-left">
-        <p class="text-primarytext">Supply: <span class="font-bold text-lg">{{ supply }}</span></p>
-        <p class="text-primarytext">Max Supply: <span class="font-bold text-lg">{{ maxSupply }}</span></p>
-        <p class="text-primarytext">Volume (24Hr): <span class="font-bold text-lg">{{ volumeUsd24Hr }}</span></p>
-        <p class="text-primarytext">Change (24Hr): <span class="font-bold text-lg">{{ changePercent24Hr }}</span></p>
-        <p class="text-primarytext">VWAP (24Hr): <span class="font-bold text-lg">{{ vwap24Hr }}</span></p>
+        <p class="text-primarytext">Supply: <span class="font-bold text-lg">{{ parseFloat(supply).toFixed(3) }}</span></p>
+        <p class="text-primarytext">Max Supply: <span class="font-bold text-lg">{{ parseFloat(maxSupply) }}</span></p>
+        <p class="text-primarytext">Volume (24Hr): <span class="font-bold text-lg">{{ parseFloat(volumeUsd24Hr).toFixed(3) }}</span></p>
+        <p class="text-primarytext">Change (24Hr): <span class="font-bold text-lg">{{ parseFloat(changePercent24Hr).toFixed(3) }} %</span></p>
+        <p class="text-primarytext">VWAP (24Hr): <span class="font-bold text-lg">{{ parseFloat(vwap24Hr).toFixed(3) }}</span></p>
         <p class="text-primarytext">Value in USD: <span class="font-bold text-lg">${{ parseFloat(priceUsd).toFixed(3) }}</span></p>
       </div>
     </div>
 
-    <!-- Add to My List Button (positioned at the top-right corner) -->
-    <button @click="addToMyList" class="absolute top-0 right-0 bg-green-500 text-white px-4 py-2 rounded-md mt-5 mr-6 transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:outline-none">Add to My List</button> <!-- Adjusted margin-top and margin-right -->
+    <button @click="addToMyList" class="absolute top-0 right-0 bg-green-500 text-white px-4 py-2 rounded-md mt-5 mr-6 transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:outline-none">Add to My List</button> 
 
-    <!-- Right Section: Buy Bitcoin -->
     <div class="text-white md:w-1/2 lg:w-1/3 xl:w-1/4">
       <div class="mb-4 text-center md:text-left">
         <p class="text-gray-600 text-sm md:text-base">Buy 1 {{ name }}</p>
-        <p class="text-2xl md:text-3xl font-bold">${{ parseFloat(priceUsd).toFixed(3) }}</p>
+        <p class="text-2xl md:text-3xl font-bold"><span class="text-green-500">$</span>{{ parseFloat(priceUsd).toFixed(3) }}</p>
       </div>
 
-      <!-- Buy Now Button -->
       <button class="bg-purple-500 text-white px-4 py-2 rounded-md w-full transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 focus:outline-none">Buy Now</button>
     </div>
   </div>
