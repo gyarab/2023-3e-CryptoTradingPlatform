147565af0e01ae771b5068087ee1ac39ad71066a
Nikola Jankovic <nikola.jankovic@student.gyarab.cz>
2024-03-30 14:37:21 +0100

    login and register page css update

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -71,7 +71,7 @@ const submit = () => {
             </div>
 
             <div class="block mt-4">
-                <label class="flex items-center text-secondarytext">
+                <label class="flex items-center text-secondarytext cursor-pointer">
                     <Checkbox name="remember" v-model:checked="form.remember" />
                     <span class="ms-2 text-sm">Remember me</span>
                 </label>
@@ -81,7 +81,7 @@ const submit = () => {
                 <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                    class="underline text-sm text-primarytext hover:hovertext rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
                     Forgot your password?
                 </Link>
